46a7c8cb42cd7095cc2de924dea49a0ec09776b2
LAPTOP-9MC6I5PL\JSRK <sivaramj83@gmail.com>
2025-05-04 21:56:51 +0530

    Fix image paths in Home and About components

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/JSRK-portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/JSRK-portfolio/' : '/',
   server: {
     port: 3000,
+    open: true
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 }) 
